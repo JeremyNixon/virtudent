78e869332c08d2023806bbba5951c2842be03d2b
Jeremy Nixon <jnixon2@gmail.com>
2015-01-18 15:14:22 -0500

    setting up production database

---
+++ app/config/production/database.php
@@ -0,0 +1,15 @@
+return array(
+
+    'connections' => array(
+
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'virtudent',
+            'username'  => 'root',
+            'password'  => 'HkjlUNvIqe',
+        ),
+
+    ),
+
+);
