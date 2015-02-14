b21218621a27f330a6d545a1577d99111aaf23cb
Jeremy Nixon <jnixon2@gmail.com>
2015-02-13 21:43:18 -0500

    styling changes

--- app/views/patient/home.blade.php
+++ app/views/patient/home.blade.php
@@ -2,9 +2,7 @@
 @extends('_patient_header')
 @section('body')
 
-<div class='patient1'></div>
-<div class='patient2'></div>
-<div class='patient3'></div>
-<div class='patient4'></div>
+<img class='img1' src='{{URL::asset('Dental-X-rays.jpg')}}'>
 
+      
 @stop
