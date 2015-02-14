05e35dd0e7e0c416fb48c4cac2b47bbbeaae2246
jeremynixon <jnixon2@gmail.com>
2015-02-14 13:33:42 -0500

    removing picture

--- app/views/patient/home.blade.php
+++ app/views/patient/home.blade.php
@@ -2,7 +2,7 @@
 @extends('_patient_header')
 @section('body')
 
-<img class='img1' src='{{URL::asset('Dental-X-rays.jpg')}}'>
-
+<!-- <img class='img1' src='{{URL::asset('Dental-X-rays.jpg')}}'>
+ -->
       
 @stop
